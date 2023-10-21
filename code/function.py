96597bc7ad46f688ab8fbf1e21ce722234469d07
Beto Avila <beto0112358@gmail.com>
2023-10-20 20:10:40 -0600

    next pull comments

--- code/function.py
+++ code/function.py
@@ -1,1 +1,10 @@
-print('hello')
+# https://stackoverflow.com/questions/19965856/how-to-get-all-comments-on-a-youtube-video
+from googleapiclient.discovery import build
+import yaml
+
+with open('./code/config.yaml', 'r') as f:
+    api_key = yaml.safe_load(f)['youtube']['API_KEY']
+with build('youtube', 'v3', developerKey=api_key) as yt_service:
+    yt_service
+# TODO pull comments from commentsthread method
+# TODO organize data pull from method
