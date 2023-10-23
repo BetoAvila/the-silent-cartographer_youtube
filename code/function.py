55dfe8c81fb43cde544722cc8ac6cb379759c67f
Beto Avila <beto0112358@gmail.com>
2023-10-22 22:11:06 -0600

    container up and running

--- code/function.py
+++ code/function.py
@@ -1,11 +1,31 @@
-# https://stackoverflow.com/questions/19965856/how-to-get-all-comments-on-a-youtube-video
-# https://stackoverflow.com/questions/70866415/how-to-install-python-specific-version-on-docker
 from googleapiclient.discovery import build
 import yaml
 
 with open('./code/config.yaml', 'r') as f:
     api_key = yaml.safe_load(f)['youtube']['API_KEY']
 with build('youtube', 'v3', developerKey=api_key) as yt_service:
-    yt_service
-# TODO pull comments from commentsthread method
-# TODO organize data pull from method
+    request = yt_service.commentThreads().list(
+        part='snippet',
+        videoId='qJsFgPpKwE0',
+        maxResults=100,
+    )
+    d = request.execute()
+total_comments = d['pageInfo']['totalResults']
+comments = [
+    d['items'][i]['snippet']['topLevelComment']['snippet']['textOriginal']
+    for i in range(total_comments)
+]
+print(total_comments, comments)
+
+# TODO organize data pulled from method (2 tables created: videos, comments)
+# variables of video:
+# id, publishedAt, title, description, duration, viewCount, likeCount, dislikeCount
+# variables of comments:
+# d['items'][i]['snippet']['topLevelComment']['snippet']['textOriginal']
+# d['items'][i]['snippet']['topLevelComment']['snippet']['authorDisplayName']
+# d['items'][i]['snippet']['topLevelComment']['snippet']['authorChannelUrl']
+# d['items'][i]['snippet']['topLevelComment']['snippet']['likeCount']
+# d['items'][i]['snippet']['topLevelComment']['snippet']['publishedAt']
+# d['items'][i]['snippet']['topLevelComment']['snippet']['updatedAt']
+
+# TODO check swarm and secrets
